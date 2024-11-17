28c08ed2e303e72355e60bb8b8717ce14a4717d5
Gr4ykt <matiilaed2@gmail.com>
2024-11-17 20:47:00 -0300

    Restricciones de rutas realizada, facilidad de aplicación, leer comentarios del archivo a nivel de raiz middleware

--- frontend/app/drive/layout.tsx
+++ frontend/app/drive/layout.tsx
@@ -6,10 +6,23 @@ import PageShared from "./shared/page";
 import PageTrash from "./trash/page";
 import SideBar from "@/components/custom/SideBar";
 import useDriveState from '@/states/useDriveState';
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function LayoutDrive() {
   const [activeView, setActiveView] = useState("mi-almacenamiento");
+  const router = useRouter();
+
+  useEffect(() => {
+    // COMPROBACION DE SI HAY INICIO DE SESION
+    console.log(document.cookie)
+    const token = document.cookie.split('; ').find(row => row.startsWith('session_jwt='));
+    
+    // EN CASO DE NO EXISTIR TOKEN, REDIRIGIR 
+    if (!token) {
+      router.push('/login');
+    }
+  }, [router]);
 
   return (
     <SidebarProvider>
---
+++ frontend/middleware.tsx
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest){
+    // OBTENIENDO LA COOKIE DEL INICIO DE SESION
+    const token = request.cookies.get('session_jwt')?.value;
+
+    // EN CASO DE NO EXISTIR TOKEN, ESTE REDIRIGIRA A login/ 
+    if(!token){
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    // SI NO SE CUMPLE LA CONDICION, SEGUIRA LA A LA REDIRECCION DE LAS RUTAS DEFINIDAS
+    return NextResponse.next();
+}
+
+// RUTAS DEFINIDAS, SERAN LAS RUTAS QUE TENDRAN RESTRICCIONES O SERAN PROTEGIDAS
+export const config = {
+    matcher: ['/drive/:path*'], // APLICAR RESTRICCION DESDE /drive Y TODAS SUS SUBRUTAS
+}
